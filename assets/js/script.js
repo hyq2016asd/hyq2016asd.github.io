c716738907e670665aa696506330de59158a3004
hyq2016asd <906765864@qq.com>
2016-07-12 16:04:18 +0800

    fast

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,11 @@ if ($(window).width() <= 1280) {
 }
 
 // Variables
-    tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
+    tag1       = $('.QQ'),
+    tag2       = $('.空间'),
+    tag3       = $('.音乐'),
+    tag4       = $('.游戏'),
+    tag5       = $('.三'),
     tag6       = $('.place_your_tag_name_here');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
